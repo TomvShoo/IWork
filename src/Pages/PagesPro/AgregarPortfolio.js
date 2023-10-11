30906456b612f0a89fd8bb014c76c0c8e1f11b46
tomas <tmshd13@gmail.com>
2023-10-11 20:29:42 -0300

    ultimos cambios

--- src/Pages/PagesPro/AgregarPortfolio.js
+++ src/Pages/PagesPro/AgregarPortfolio.js
@@ -1,33 +1,57 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BarraMenuPro from '../../components/BarraMenuPro';
 import { Card } from 'primereact/card';
 import { Button } from "primereact/button";
 import { InputTextarea } from 'primereact/inputtextarea';
 import "../../style.css";
+import jwt_decode from 'jwt-decode';
 
 const AgregarPortfolio = () => {
   
   const [images, setImages] = useState([]);
   const [description, setDescription] = useState('');
   const [certificates, setCertificates] = useState('');
+  const [professionalId, setProfessionalId] = useState('');
+
+  useEffect(() => {
+    // obtener token 
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        // decodificar token
+        const decodedToken = jwt_decode(token);
+        setProfessionalId(decodedToken.usuarioId);
+        // const professionalId = decodedToken.usuarioId;
+        // setProfessionalId(decodedToken.profesionalId);
+    }
+  })
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
 
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        const base64Image = event.target.result;
-        setImages([...images, base64Image]);
-      };
-      reader.readAsDataURL(file);
+    if (images.length < 3) {
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const base64Image = event.target.result;
+          setImages([...images, base64Image]);
+        };
+        reader.readAsDataURL(file);
+      }
     }
   };
 
   const handleSave = () => {
+
+    const data = {
+        description,
+        certificates,
+        images,
+        professionalId,
+    }
     console.log('Imagenes', images);
     console.log('Descripcion', description);
     console.log('Certificados', certificates);
+    console.log('ID del Profesional', professionalId);
   }
 
   const footer = (
@@ -69,7 +93,7 @@ const AgregarPortfolio = () => {
                                     </div>
                                     )}
                                     <input type="file" accept="image/*" onChange={handleImageUpload} />
-                                    <Button label="Subir imagenes" severity="secondary" text />
+                                    <Button label="Subir imagenes" severity="secondary" text type="file" accept="image/*" onChange={handleImageUpload}/>
                                 </div>    
                                 </Card>
                             </Card>
--- src/Pages/PagesPro/PerfilProfesional.js
+++ src/Pages/PagesPro/PerfilProfesional.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "primereact/card";
 import Calificacion from "../../components/Rating";
 import { Avatar } from "primereact/avatar";
@@ -6,6 +6,7 @@ import { Button } from "primereact/button";
 import { Link } from "react-router-dom";
 import BarraMenu from "../../components/BarraMenuPro"
 import BotonesRedes from "../../components/BotonesRedes";
+import axios from 'axios';
 // Estilos
 import "../../style.css";
 import ImageCarousel from "../../components/Carrusel";
@@ -37,6 +38,30 @@ const Estilo = {
 };
 
 export const PerfilPro = () => {
+
+  const [usuario, setUsuario] = useState(null);
+
+  useEffect(() => {
+    // obtener el token del almacenamiento local
+    const token = localStorage.getItem('token');
+
+    if(token) {
+      //se realiza la solicitud al servidor 
+      axios.get('http://localhost:4000/auth/perfil', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setUsuario(response.data);
+      })
+      .catch((error) => {
+        console.error('error al obtener los datos del usuario', error);
+      });
+    }
+
+  })
+
   return (
     <div>
       <BarraMenu />
@@ -45,10 +70,12 @@ export const PerfilPro = () => {
             <div className="info">
               <Avatar label="P" size="xlarge" />
             </div>
-            <div>
-              <h3>Nombre Apellido</h3>
-              <h4>Profesión</h4>
-            </div>
+            {usuario && (
+              <div>
+                <h3>{usuario.nombre}</h3>
+                <h4>Profesión</h4>
+              </div>
+            )}
             <div>
               <Calificacion />
             </div>
