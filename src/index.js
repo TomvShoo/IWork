02c25bc8435efead659f2da084a6424c44c1f3fe
ReneRetamales <reneretamalespinto1@gmail.com>
2023-11-15 16:23:20 -0300

    Service worker agregado. iWork ya es APW

--- src/index.js
+++ src/index.js
@@ -11,3 +11,20 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+// Para el service worker: esto lo registra
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/service-worker.js").then(
+      function (registration) {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
