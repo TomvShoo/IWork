e174594e0fd99b219de3f4625b216d7508a5a385
tomas <tmshd13@gmail.com>
2023-11-22 23:10:23 -0300

    error 3

--- src/index.js
+++ src/index.js
@@ -13,18 +13,18 @@ root.render(
 );
 
 // Para el service worker: esto lo registra
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      function (registration) {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      function (err) {
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", function () {
+//     navigator.serviceWorker.register("/service-worker.js").then(
+//       function (registration) {
+//         console.log(
+//           "ServiceWorker registration successful with scope: ",
+//           registration.scope
+//         );
+//       },
+//       function (err) {
+//         console.log("ServiceWorker registration failed: ", err);
+//       }
+//     );
+//   });
+// }
